15e115d1fd8842b44068ef1872d7e41b274a4ca4
Felipe - Notebook <sharpion.k@gmail.com>
2019-09-05 21:58:19 +0100

    Adding bank accounts

--- src/sections/gifts.jsx
+++ src/sections/gifts.jsx
@@ -13,6 +13,31 @@ class Gifts extends React.Component {
                 <p className="regular-text bold hide-on-mobile"><a target="_blank" rel="noopener noreferrer" href="https://sites.icasei.com.br/felipegiovanna/pt_br/store/9/1/1">Lista de Presentes</a></p>
                 <p className="regular-text bold hide-on-desktop"><a href="https://sites.icasei.com.br/felipegiovanna/pt_br/store/9/1/1">Lista de Presentes</a></p>
                 <br />
+                <p className="regular-text bold">Atendendo a pedidos, podes também fazer o depósito direto em nossas contas bancárias!</p>
+                <div className="full-width accounts-cointainer">
+                    <div className="account regular-text">
+                        <b>Banco do Brasil (001)</b><br />
+                        Felipe Zanon do Nascimento<br />
+                        CPF 050 143 229 99<br />
+                        Agência 0982-2<br />
+                        C/C 47930-6
+                    </div>
+                    <div className="account regular-text">
+                        <b>Nu Bank (260)</b><br />
+                        Felipe Zanon do Nascimento<br />
+                        CPF 050 143 229 99<br />
+                        Agência 0001<br />
+                        C/C 426087-4
+                    </div>
+                    <div className="account regular-text">
+                        <b>Itaú (341)</b><br />
+                        Giovanna De Marchi Capeletto<br />
+                        CPF 081 691 819 84<br />
+                        Agência 9145<br />
+                        C/C 00591-5                       
+                    </div>
+                </div>
+                <br />
                 <p className="regular-text">Agradecemos a lembrança <span role="img" aria-label="heart">💖</span></p>
             </div>
         )
